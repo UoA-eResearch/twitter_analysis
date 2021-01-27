8fa7d02e66f6be8aab1427ad41517e3c0cc4ae59
Nick Young <nick.young@auckland.ac.nz>
2021-01-28 11:15:41 +1300

    simplify botscores.R, write csv every 100 reuslts, ensure user_id doesn't become scientific notation

--- botscores.R
+++ botscores.R
@@ -5,54 +5,31 @@ library(tweetbotornot2)
 token = readRDS("twittertoken.rds")
 rtweet::rate_limit(token, "get_timeline")
 
-df = read.csv("data/botscores.csv")
-
-ids = read.csv("data/users_ids.csv")
+df = read.csv("data/botscores.csv", colClasses = list(user_id='character'))
+ids = read.csv("data/users_ids.csv", colClasses = list(user_id='character'))
 ids = ids[!ids$user_id %in% df$user_id,] # Filter out users that have already been checked
-
-ids$user_id = as.character(ids$user_id)
 
 print("CSVs loaded")
 
-## convert users vector (user IDs or screen names) a list of 10-user chunks
-users <- chunk_users(ids$user_id, n = 10)
-print("Chunked user vector created")
-
-## initialize output vector
-output <- vector("list", length(users))
-
-for (i in seq_along(output)) {
-  ## set oops counter
-  oops <- 0L
-
-  ## check rate limit- if < 10 calls remain, sleep until reset
-  print(rl <- rtweet::rate_limit(query = "get_timelines", token=token))
-  while (rl[["remaining"]] < 10L) {
-    ## prevent infinite loop
-    oops <- oops + 1L
-    if (oops > 3L) {
-      stop("rate_limit() isn't returning rate limit data", call. = FALSE)
-    }
-    cat("Sleeping for", round(max(as.numeric(rl[["reset"]], "mins"), 0.5), 1), "minutes...")
-    Sys.sleep(max(as.numeric(rl[["reset"]], "secs"), 30))
-    rl <- rtweet::rate_limit(query = "get_timelines", token=token)
-  }
-
-  ## get bot estimates
-  output[[i]] <- predict_bot(users[[i]], token = token)
-
-  ## print iteration update
-  cat(sprintf("%d / %d (%.f%%)\n", i, length(output), i / length(output) * 100))
+save = function() {
+  df = df[order(-df$n_tweets),] # Order by n_tweets descending
+  print(df)
+  bots = sum(df$prob_bot > .5, na.rm=T)
+  ppl = sum(df$prob_bot < .5, na.rm=T)
+  print(sprintf("%d bots, %d ppl, %f%% bots", bots, ppl, bots / ppl * 100))
+  
+  write.csv(df, "data/botscores.csv", row.names=F)
 }
 
-## merge into single data table
-output = do.call("rbind", output) # Bind into one dataframe
-new_df = merge(output, ids, by="user_id") # Join back n_tweets
-df = rbind(df, new_df) # Add back in existing results
-df = df[order(-df$n_tweets),] # Order by n_tweets descending
-print(df)
-bots = sum(df$prob_bot > .5, na.rm=T)
-ppl = sum(df$prob_bot < .5, na.rm=T)
-print(sprintf("%d bots, %d ppl, %f%% bots", bots, ppl, bots / ppl * 100))
+for (i in 1:nrow(ids)) {
+  output = predict_bot(ids$user_id[i], token = token)
+  output$n_tweets = ids$n_tweets[i]
+  df = rbind(df, output)
 
-write.csv(df, "data/botscores.csv", row.names=F)
+  ## print iteration update
+  cat(sprintf("%d / %d (%.f%%)\n", i, nrow(ids), i / nrow(ids) * 100))
+  if (i %% 100 == 0) {
+    save()
+  }
+}
+
