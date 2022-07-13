de0102c2ae8098632683b3c45a9d65e07a868bd7
Nick Young <nick.young@auckland.ac.nz>
2022-07-13 12:40:56 +1200

    update README, twitter API resumability

--- auto.sh
+++ auto.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 start='2006-11-30'
-end='2021-01-14'
+end='2021-06-21'
 
 start=$(date -d $start +%Y%m%d)
 end=$(date -d $end +%Y%m%d)
@@ -8,7 +8,13 @@ end=$(date -d $end +%Y%m%d)
 while [[ $start -lt $end ]]
 do
 	bd=$(date -d"$end - 1 day" +"%Y-%m-%d")
-	ed=$(date -d"$end" +"%Y-%m-%d")
-	twarc2 search '"climate change" OR "global warming" OR "sea level rise" OR "climatechange" OR "globalwarming" OR "sealevelrise"' --archive --start-time $bd --end-time $ed data/from_research_API/$bd.jsonl
+        ed=$(date -d"$end" +"%Y-%m-%d")
+	FILE=data/from_research_API/$bd.jsonl
+	if [ -f "$FILE" ]; then
+		echo "$FILE exists, skipping"
+	else
+		echo "$FILE does not exist, running twarc2"
+		twarc2 search '"climate change" OR "global warming" OR "sea level rise" OR "climatechange" OR "globalwarming" OR "sealevelrise"' --archive --start-time $bd --end-time $ed $FILE
+	fi
         end=$(date -d"$end - 1 day" +"%Y%m%d")
 done
